4d93e94b3c6f29485924f4cb3423ff22275e71a6
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-09-21 00:32:05 +0900

    update:アイコン用のマスターデータ追加

--- Assets/_CryStar/_ROTL/Runtime/Scripts/Generated/MasterBattleCharacter.cs
+++ Assets/_CryStar/_ROTL/Runtime/Scripts/Generated/MasterBattleCharacter.cs
@@ -5,13 +5,15 @@ public static class MasterBattleCharacter
     private static readonly Dictionary<int, string> _name = new Dictionary<int, string>()
     {
         { 1, "ステージ" },
-        { 2, "フロース" }
+        { 2, "フロース" },
+        { 3, "スピーカー"}
     };
     
     private static readonly Dictionary<int, string> _iconPath = new Dictionary<int, string>()
     {
-        { 1, "Assets/AssetStoreTools/Images/Battle/Character/stage.png" },
-        { 2, "Assets/AssetStoreTools/Images/Battle/Character/flos.png" }
+        { 1, "Assets/AssetStoreTools/Images/Battle/Character/Stage_I.png" },
+        { 2, "Assets/AssetStoreTools/Images/Battle/Character/flos.png" },
+        { 3, "Assets/AssetStoreTools/Images/Battle/Character/Speaker_I.png" }
     };
     
     public static string GetName(int id) => _name[id];
