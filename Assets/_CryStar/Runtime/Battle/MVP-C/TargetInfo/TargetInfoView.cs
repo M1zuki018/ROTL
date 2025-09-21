7119248e4c4d9117f1da5741beb3a9b3672c5b1d
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-09-21 13:35:42 +0900

    add:使用分のSerializeField

--- Assets/_CryStar/Runtime/Battle/MVP-C/TargetInfo/TargetInfoView.cs
+++ Assets/_CryStar/Runtime/Battle/MVP-C/TargetInfo/TargetInfoView.cs
@@ -8,6 +8,42 @@ namespace CryStar.CommandBattle
     public class TargetInfoView : MonoBehaviour
     {
         /// <summary>
+        /// ターゲット情報を表示するUI群を管理するクラス
+        /// </summary>
+        [SerializeField] 
+        private UIContents_TargetInfo _infomation;
+        
+        /// <summary>
+        /// ターゲットの名前を表示するUI群を管理するクラス
+        /// </summary>
+        [SerializeField]
+        private UIContents_TargetName _name;
+
+        /// <summary>
+        /// 左上の敵の分析テキストの参照
+        /// </summary>
+        [SerializeField] 
+        private CustomText _analysis;
+
+        /// <summary>
+        /// 左上のUI群のCanvasGroup
+        /// </summary>
+        [SerializeField] 
+        private CanvasGroup _upperLeftCanvasGourp;
+
+        /// <summary>
+        /// キャラクターの立ち絵を表示するコンポーネントの参照
+        /// </summary>
+        [SerializeField] 
+        private CustomImage _character;
+        
+        /// <summary>
+        /// キャラクターの立ち絵を表示するUI群のCanvasGroup
+        /// </summary>
+        [SerializeField]
+        private CanvasGroup _characterCanvasGroup;
+        
+        /// <summary>
         /// Setup
         /// </summary>
         public void Setup()
